98eba1591e5e381d9f8dda6ae2ba7a68f1103ee1
JongPyoAhn <whdvy95@gmail.com>
2023-05-25 17:18:46 +0900

    feature: HomeDroppedTableViewCell 오토레이아웃 적용

--- Kreamish/Kreamish/Scenes/HomeScene/HomeDropped/HomeDroppedTableViewCell.swift
+++ Kreamish/Kreamish/Scenes/HomeScene/HomeDropped/HomeDroppedTableViewCell.swift
@@ -40,21 +40,52 @@ final class HomeDroppedTableViewCell: UITableViewCell {
 }
 extension HomeDroppedTableViewCell {
     private func configureUI() {
+        self.contentView.addSubview(self.mainLabel)
+        self.mainLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        self.contentView.addSubview(self.subLabel)
+        self.subLabel.snp.makeConstraints {
+            $0.top.equalTo(self.mainLabel.snp.bottom)
+            $0.leading.equalTo(self.mainLabel.snp.leading)
+            $0.trailing.equalTo(self.mainLabel.snp.trailing)
+        }
         self.contentView.addSubview(self.collectionView)
-        
+        self.collectionView.snp.makeConstraints {
+            $0.top.equalTo(self.subLabel.snp.bottom).offset(13)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
     }
 }
-
 extension HomeDroppedTableViewCell: UICollectionViewDelegateFlowLayout {
-    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumInteritemSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        return 1
+    }
 }
 extension HomeDroppedTableViewCell: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
         0
     }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: "HomeDroppedCollectionViewCell", for: indexPath
+            ) as? HomeDroppedCollectionViewCell else { return UICollectionViewCell() }
+//        cell.setUp(<#T##goodsImage: UIImage##UIImage#>, <#T##goodsName: String##String#>, <#T##goodsDescription: String##String#>, <#T##goodsPrice: String##String#>)
+        return cell
     }
-    
 }

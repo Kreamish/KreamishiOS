85e6166379424b127124bb1dfcf48ec46173e0f4
MiyoLee <iimiyo@naver.com>
2023-05-30 21:34:10 +0900

    상품 collectionView 정리

--- Kreamish/Kreamish/Scenes/ShopScene/ShopContent/ProductCollection/ProductViewController.swift
+++ Kreamish/Kreamish/Scenes/ShopScene/ShopContent/ProductCollection/ProductViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ProductCollectionViewController.swift
+//  ProductViewController.swift
 //  Kreamish
 //
 //  Created by Miyo Lee on 2023/05/18.
@@ -9,36 +9,35 @@ import UIKit
 
 import SnapKit
 
-class ProductCollectionViewController: UIViewController {
+class ProductViewController: UIViewController {
     
     // swiftlint:disable line_length
     private let productList: [Product] = [
-        Product(englishName: "LANCER Mens ACTIVE-100 Blue Running Shoe - 7 UK (ACTIVE-100NBL-MSTD-7)", brand: "Reebok", category: "Shoes", price: "119900", imgUrl: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81K-j6yqNXL._AC_UL320_.jpg"),
-        Product(englishName: "Wilson mens KAOS Stroke 2.0 India Ink/White/Vivid Blue Tennis Shoe - 10 UK (WRS328850U105)", brand: "New Balance", category: "Shoes", price: "999900", imgUrl: "https://m.media-amazon.com/images/I/81uis1XGjKL._AC_UL320_.jpg"),
-        Product(englishName: "Men's Jordan 1 Retro High OG Rebellionaire Blk/Wht-Particle Grey (555088 036)", brand: "Nike", category: "Shoes", price: "6299300", imgUrl:"https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61HcA++bNJL._AC_UL320_.jpg"),
+        Product(englishName: "LANCER Mens ACTIVE-100 Blue Running Shoe - 7 UK (ACTIVE-100NBL-MSTD-7)", brand: "Reebok", category: "Shoes", price: "119,900", imgUrl: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81K-j6yqNXL._AC_UL320_.jpg"),
+        Product(englishName: "Wilson mens KAOS Stroke 2.0 India Ink/White/Vivid Blue Tennis Shoe - 10 UK (WRS328850U105)", brand: "New Balance", category: "Shoes", price: "999,900", imgUrl: "https://m.media-amazon.com/images/I/81uis1XGjKL._AC_UL320_.jpg"),
+        Product(englishName: "Men's Jordan 1 Retro High OG Rebellionaire Blk/Wht-Particle Grey (555088 036)", brand: "Nike", category: "Shoes", price: "6,299,300", imgUrl:"https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61HcA++bNJL._AC_UL320_.jpg"),
         Product(englishName: "Nike Mens Jordan 3 Retro Basketball Shoes", brand: "Skechers", category: "Shoes", price: "6299300", imgUrl: "https://m.media-amazon.com/images/I/51tWQy69c6L._AC_UL320_.jpg"),
-        Product(englishName: "Nike Men's Shoes Jordan 1 High Zoom Air CMFT Black Monarch CT0978-002", brand: "Nike", category: "Shoes", price: "6299000", imgUrl: "https://m.media-amazon.com/images/I/61IgNNMa0ZL._AC_UL320_.jpg"),
+        Product(englishName: "Nike Men's Shoes Jordan 1 High Zoom Air CMFT Black Monarch CT0978-002", brand: "Nike", category: "Shoes", price: "6,299,000", imgUrl: "https://m.media-amazon.com/images/I/61IgNNMa0ZL._AC_UL320_.jpg"),
         Product(englishName: "Nike Jordan 1 Mid Pink Black", brand: "Under Armour", category: "Shoes", price: "6121000", imgUrl: "https://m.media-amazon.com/images/I/71cBbSQiQ0L._AC_UL320_.jpg"),
-        Product(englishName: "LANCER Mens ACTIVE-100 Blue Running Shoe - 7 UK (ACTIVE-100NBL-MSTD-7)", brand: "Reebok", category: "Shoes", price: "119900", imgUrl: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81K-j6yqNXL._AC_UL320_.jpg"),
-        Product(englishName: "Wilson mens KAOS Stroke 2.0 India Ink/White/Vivid Blue Tennis Shoe - 10 UK (WRS328850U105)", brand: "New Balance", category: "Shoes", price: "999900", imgUrl: "https://m.media-amazon.com/images/I/81uis1XGjKL._AC_UL320_.jpg"),
-        Product(englishName: "Men's Jordan 1 Retro High OG Rebellionaire Blk/Wht-Particle Grey (555088 036)", brand: "Nike", category: "Shoes", price: "6299300", imgUrl:"https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61HcA++bNJL._AC_UL320_.jpg"),
+        Product(englishName: "LANCER Mens ACTIVE-100 Blue Running Shoe - 7 UK (ACTIVE-100NBL-MSTD-7)", brand: "Reebok", category: "Shoes", price: "119,900", imgUrl: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81K-j6yqNXL._AC_UL320_.jpg"),
+        Product(englishName: "Wilson mens KAOS Stroke 2.0 India Ink/White/Vivid Blue Tennis Shoe - 10 UK (WRS328850U105)", brand: "New Balance", category: "Shoes", price: "999,900", imgUrl: "https://m.media-amazon.com/images/I/81uis1XGjKL._AC_UL320_.jpg"),
+        Product(englishName: "Men's Jordan 1 Retro High OG Rebellionaire Blk/Wht-Particle Grey (555088 036)", brand: "Nike", category: "Shoes", price: "6,299,300", imgUrl:"https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61HcA++bNJL._AC_UL320_.jpg"),
         Product(englishName: "Nike Mens Jordan 3 Retro Basketball Shoes", brand: "Skechers", category: "Shoes", price: "6299300", imgUrl: "https://m.media-amazon.com/images/I/51tWQy69c6L._AC_UL320_.jpg"),
-        Product(englishName: "Nike Men's Shoes Jordan 1 High Zoom Air CMFT Black Monarch CT0978-002", brand: "Nike", category: "Shoes", price: "6299000", imgUrl: "https://m.media-amazon.com/images/I/61IgNNMa0ZL._AC_UL320_.jpg"),
-        Product(englishName: "Nike Jordan 1 Mid Pink Black", brand: "Under Armour", category: "Shoes", price: "6121000", imgUrl: "https://m.media-amazon.com/images/I/71cBbSQiQ0L._AC_UL320_.jpg")
+        Product(englishName: "Nike Men's Shoes Jordan 1 High Zoom Air CMFT Black Monarch CT0978-002", brand: "Nike", category: "Shoes", price: "6,299,000", imgUrl: "https://m.media-amazon.com/images/I/61IgNNMa0ZL._AC_UL320_.jpg"),
+        Product(englishName: "Nike Jordan 1 Mid Pink Black", brand: "Under Armour", category: "Shoes", price: "6,121,000", imgUrl: "https://m.media-amazon.com/images/I/71cBbSQiQ0L._AC_UL320_.jpg")
     ]
     // swiftlint:disable line_length
     
     lazy var productCollectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.scrollDirection = .vertical
-        
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
         return collectionView
     }()
     
     private func configure() {
         productCollectionView.allowsSelection = true
-        productCollectionView.backgroundColor = .systemGray3
+        productCollectionView.backgroundColor = .white
         
         productCollectionView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(10)
@@ -61,7 +60,7 @@ class ProductCollectionViewController: UIViewController {
     
 }
 
-extension ProductCollectionViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+extension ProductViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     
     // UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -86,21 +85,21 @@ extension ProductCollectionViewController: UICollectionViewDataSource, UICollect
     }
 }
 
-extension ProductCollectionViewController: UICollectionViewDelegateFlowLayout {
+extension ProductViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let itemSpacing: CGFloat = 10
+        let itemSpacing: CGFloat = 5
         //            let lineSpacing: CGFloat = 10
         let width: CGFloat = (collectionView.bounds.width - itemSpacing) / 2
-        let height: CGFloat = width * 1.75
+        let height: CGFloat = width * 2.0
         return CGSize(width: width, height: height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
+        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
+        return 0
     }
 }
 
--- Kreamish/Kreamish/Scenes/ShopScene/ShopContent/ShopContentTableViewController.swift
+++ Kreamish/Kreamish/Scenes/ShopScene/ShopContent/ShopContentTableViewController.swift
@@ -2,10 +2,19 @@ import UIKit
 
 class ShopContentTableViewController: UITableViewController {
     
+    private let category: String    //enum으로 바꿔보기
+    init(category: String) {
+        self.category = category
+        super.init(style: .plain)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray6
-        
+        view.backgroundColor = .systemBackground
         tableView.register(ShopContentTableViewCell.self, forCellReuseIdentifier: ShopContentTableViewCell.id)
     }
     
@@ -16,24 +25,35 @@ class ShopContentTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1 // Return the number of rows you want to display
+        return 2 // Return the number of rows you want to display
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 4000  // Set the desired height for your cells
+        switch indexPath.row {
+        case 0: return 200
+        case 1: return 2500
+        default: return 0
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         if let cell = tableView.dequeueReusableCell(withIdentifier: ShopContentTableViewCell.id, for: indexPath) as? ShopContentTableViewCell {
             cell.layoutIfNeeded()
             
-            let productViewController = ProductCollectionViewController()
-            self.addChild(productViewController)
-            cell.addSubview(productViewController.view)
-            
-            productViewController.didMove(toParent: self)
-            productViewController.view.layoutIfNeeded()
-            
+            switch indexPath.row {
+            case 0: //서브카테고리 collection view
+                let subCategoryViewController = UICollectionViewController()
+            case 1: //상품 collection view
+                let productViewController = ProductViewController()
+                self.addChild(productViewController)
+                cell.addSubview(productViewController.view)
+                
+                productViewController.didMove(toParent: self)
+                productViewController.view.layoutIfNeeded()
+            default:
+                print("default")
+            }
             return cell
         } else {
             fatalError("DequeueReusableCell failed while casting")
